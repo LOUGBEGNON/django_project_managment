e250e6c3dca5355984b516e581a82a531fe18b54
Takouchouang <takouchouangfrede@gmail.com>
2024-09-27 14:50:44 +0700

    Formulaire

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,8 +1,12 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UsernameField, PasswordChangeForm, PasswordResetForm, SetPasswordForm
+from django.contrib.auth.forms import (AuthenticationForm, PasswordChangeForm,
+                                       PasswordResetForm, SetPasswordForm,
+                                       UserCreationForm, UsernameField)
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+
 from apps.users.models import Profile
+
 
 class SigninForm(AuthenticationForm):
     username = UsernameField(widget=forms.TextInput(attrs={
@@ -22,17 +26,21 @@ class SigninForm(AuthenticationForm):
 
 
 class SignupForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ('username', 'email', )
+   class Meta:
+     model = User
+   fields = ('username', 'email', )
 
-    def __init__(self, *args, **kwargs):
-        super(SignupForm, self).__init__(*args, **kwargs)
+   def __init__(self, *args, **kwargs):
+       super(SignupForm, self).__init__(*args, **kwargs)
 
-        for field_name, field in self.fields.items():
-            self.fields[field_name].widget.attrs['placeholder'] = field.label
-            self.fields[field_name].widget.attrs['class'] = 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
-            self.fields[field_name].widget.attrs['required'] = True
+       for field_name, field in self.fields.items():
+           self.fields[field_name].widget.attrs['placeholder'] = field.label
+           self.fields[field_name].widget.attrs['class'] = 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
+           self.fields[field_name].widget.attrs['required'] = True
+
+
+
+
 
 
 
@@ -66,13 +74,21 @@ class UserPasswordChangeForm(PasswordChangeForm):
         'class': 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500', 
         'placeholder': 'Confirm New Password'
     }), label="Confirm New Password")
-
+    
 
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ('user', 'role', 'avatar',)
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            self.fields[field_name].widget.attrs['placeholder'] = field.label
+            self.fields[field_name].widget.attrs['class'] = 'shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
+            self.fields[field_name].widget.attrs['required'] = False        exclude = ('user', 'role', 'avatar',)
 
     def __init__(self, *args, **kwargs):
         super(ProfileForm, self).__init__(*args, **kwargs)
